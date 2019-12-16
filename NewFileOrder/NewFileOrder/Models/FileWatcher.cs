8cd929af0c7510fe7e106b376bb658e6d4c2b2b0
JanPro <janproxdd@gmail.com>
2019-12-16 16:31:42 +0100

    Another model update

---
+++ NewFileOrder/NewFileOrder/Models/FileWatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NewFileOrder.Models
+{
+    class FileWatcher
+    {
+        public event FileSystemEventHandler OnChanged;
+        public event FileSystemEventHandler OnCreated;
+        public event FileSystemEventHandler OnDeleted;
+        public event RenamedEventHandler OnRenamed;
+        private FileSystemWatcher _filesWatcher;
+    }
+}
---
+++ NewFileOrder/NewFileOrder/Models/Managers/TagManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewFileOrder.Models.Managers
+{
+    class TagManager:Manager
+    {
+        public TagManager(MyDbContext dbContext) : base(dbContext) { }
+    }
+}
